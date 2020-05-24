2319b5b73973df012d57b31dd84c204b67ab7445
Dinesh Kumar <dineshkumar.software@gmail.com>
2020-05-24 11:53:14 +0530

    Fixed Templates

--- ShoppinSIte/settings.py
+++ ShoppinSIte/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ShoppinSIte.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "Templates")],
+        'DIRS': [os.path.join(BASE_DIR, "ShoppinSIte/Templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
