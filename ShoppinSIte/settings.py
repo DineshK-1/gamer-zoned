e3206e796507c1f1cae5c553c1dcddb8f6397783
Dinesh Kumar <dineshkumar.software@gmail.com>
2020-05-30 21:56:57 +0530

    Pog

--- ShoppinSIte/settings.py
+++ ShoppinSIte/settings.py
@@ -122,11 +122,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-#Security
-
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
